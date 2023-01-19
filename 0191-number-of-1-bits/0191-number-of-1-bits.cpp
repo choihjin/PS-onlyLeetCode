4b142b41b964b99a9d831a98f4e3b9e8c927a425
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-01-19 16:04:00 +0900

    Time: 0 ms (100.00%), Space: 6 MB (67.89%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,7 +3,6 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n > 0) {
-            cout << n << endl;
             if(n & 1) count++; // bitwise and
             n >>= 1; // n = n >> 1
         }
